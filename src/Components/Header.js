c4a72680ce40ab4f4be86e56788ce52fb15e66eb
Flora <mazzavillanif@gmail.com>
2021-08-11 20:41:00 +1000

    changed logo, icon, footer/header color, carousel in homepage

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,9 +12,11 @@ export function Header( props ) {
     })
     return (
                                                 //colours for header (words and background)
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-primary bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">{props.name}</a>
+                <a className="navbar-brand" href="#">
+                    <img src="/images/logobrand.png"></img> 
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -24,7 +26,7 @@ export function Header( props ) {
                     </ul>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
                     </form>
                 </div>
             </div>
