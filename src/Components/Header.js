8af007048ca2e5bcaa3e6af26038ff21ca3440b8
Esteban <cehernandezco@gmail.com>
2021-08-12 21:12:29 +1000

    logo brand smaller

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ export function Header( props ) {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
-                    <img src="/images/logobrand.png"></img> 
+                    <img src="/images/logobrand.png" height="60"></img> 
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
